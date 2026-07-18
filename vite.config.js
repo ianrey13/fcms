03aca55ddc48f2ba16678304d7f02ade7b56b665
ianrey13Devs <iubaub4@gmail.com>
2026-07-18 19:18:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/main.jsx',
+            input: [
+                'resources/css/app.css',
+                'resources/js/main.jsx'
+            ],
             refresh: true,
         }),
         react(),
@@ -22,10 +25,19 @@ export default defineConfig({
         outDir: 'public/build',
         manifest: true,
         rollupOptions: {
-            external: ['react-is'],
             output: {
-                manualChunks: {
-                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+                            return 'vendor';
+                        }
+                        if (id.includes('recharts')) {
+                            return 'recharts';
+                        }
+                        if (id.includes('leaflet')) {
+                            return 'leaflet';
+                        }
+                    }
                 },
             },
         },
